66d02d6f917356c5ade2db2ff6cbe1ca89bbe468
Ganiat Badara <ganiyatbadara@gmail.com>
2021-12-03 20:24:23 +0000

    Fix undefined bug and make options show properly

--- assets/js/script.js
+++ assets/js/script.js
@@ -112,7 +112,7 @@ getNewQuestion = () => {
 
     questionCounter++
     progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
-    progressBarFull.style.width = `${(questionCounter/MAX_QUESTIONS) * 10}%`
+    progressBarFull.style.width = `${(questionCounter/MAX_QUESTIONS) * 100}%`
 
     const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
     currentQuestion = availableQuestions[questionsIndex]
@@ -120,7 +120,7 @@ getNewQuestion = () => {
 
     choices.forEach(choice => {
         const number = choice.dataset["number"]
-        choice.innerText = currentQuestion["choice + number"]
+        choice.innerText = currentQuestion["choice" + number]
     })
 
     availableQuestions.splice(questionsIndex, 1)

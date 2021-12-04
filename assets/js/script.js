2ce821a143f70afc24ac9fbc427572f8b1a96f8d
Ganiat Badara <ganiyatbadara@gmail.com>
2021-12-04 21:22:00 +0000

    changes to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,8 @@ const choices = Array.from(document.querySelectorAll(".choice-text"));
 const progressText = document.querySelector("#progressText");
 const scoreText = document.querySelector("#score");
 const progressBarFull = document.querySelector("#progressBarFull");
+const levels = document.querySelector(".level")
+const body = document.querySelector("body")
 
 let currentQuestion = {}
 let acceptingAnswers = true
@@ -10,8 +12,7 @@ let score = 0
 let questionCounter = 0
 let availableQuestions = []
 
-let questions = [
-    {
+let easyQuestions = [{
         question: "What is JavaScript?",
         choice1: "A Web Browser",
         choice2: "A Programming/Scripting Language",
@@ -93,18 +94,129 @@ let questions = [
     },
 ]
 
+let mediumQuestions = [{
+        question: "How do you create a function in JavaScript?",
+        choice1: "function myFunction()",
+        choice2: "function:myFunction()",
+        choice3: "function = myFunction()",
+        choice4: "function; myFunction()",
+        answer: 1,
+    },
+    {
+        question: "How do you call a function named 'myFunction'?",
+        choice1: "callMyFunction()",
+        choice2: "call myFunction()",
+        choice3: "call function myFunction()",
+        choice4: "myFunction()",
+        answer: 4,
+    },
+    {
+        question: 'How do you write "Hello World" in an alert box?',
+        choice1: 'msgBox("Hello World");',
+        choice2: 'alert("Hello World"); ',
+        choice3: 'alertBox("Hello World");',
+        choice4: 'msg("Hello World");',
+        answer: 2,
+    },
+    {
+        question: "How can you add a comment in a JavaScript?",
+        choice1: "//This is a comment",
+        choice2: "<!--This is a comment-->",
+        choice3: "'This is a comment",
+        choice4: "'This is a comment//",
+        answer: 1,
+    },
+    {
+        question: "How to insert a comment that has more than one line?",
+        choice1: "/This comment has more than one line / ",
+        choice2: "//This comment has more than one line //",
+        choice3: "<!--This comment has more than one line-- > ",
+        choice4: "/*This comment has more than one line * /",
+        answer: 4,
+    },
+    {
+        question: "How do you declare a JavaScript variable?",
+        choice1: "v carName;",
+        choice2: "variable carName;",
+        choice3: "var carName; ",
+        choice4: "variableCarName;",
+        answer: 3,
+    },
+    {
+        question: "Which operator is used to assign a value to a variable?",
+        choice1: "=",
+        choice2: "x",
+        choice3: "-",
+        choice4: "*",
+        answer: 1,
+    },
+    {
+        question: "Which operator is used to assign a value to a variable?",
+        choice1: "=",
+        choice2: "x",
+        choice3: "-",
+        choice4: "*",
+        answer: 1,
+    },
+    {
+        question: "What will the following code return: Boolean(10 > 9)",
+        choice1: "True",
+        choice2: "False",
+        choice3: "NaN",
+        choice4: "Null",
+        answer: 1,
+    },
+    {
+        question: "Floating Point Numbers can also be called...",
+        choice1: "Whole numbers",
+        choice2: "Decimal numbers",
+        choice3: "Even numbers",
+        choice4: "Odd numbers",
+        answer: 2,
+    },
+]
+
+
+let hardQuestions = [
+
+]
+
 const SCORE_POINTS = 10
 const MAX_QUESTIONS = 10
 
 startGame = () => {
+    let result = []
+    body.addEventListener("click", (event) => {
+        if (event.target.id === easy) {
+            result = [...easyQuestions]
+        } else if (event.target.id === medium) {
+            result = [...mediumQuestions]
+        } else {
+            result = [...hardQuestions]
+        } console.log(result)
+    
+    })
     questionCounter = 0
     score = 0
-    availableQuestions = [...questions]
-    getNewQuestion()
+    getNewQuestion(result)
 }
 
-getNewQuestion = () => {
-    if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
+// selectLevel = () => {
+//     let availableQuestions
+//     body.addEventListener("click", (event) => {
+//         if (event.target.id === easy) {
+//             availableQuestions = [...easyQuestions]
+//         } else if (event.target.id === medium) {
+//             availableQuestions = [...mediumQuestions]
+//         } else {
+//             availableQuestions = [...hardQuestions]
+//         } console.log(availableQuestions)
+//         return availableQuestions
+//     })
+// }
+
+getNewQuestion = (availableQuestions) => {
+    if (availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
         localStorage.setItem("mostRecentScore", score)
 
         return window.location.assign("/end.html")
@@ -130,7 +242,7 @@ getNewQuestion = () => {
 
 choices.forEach(choice => {
     choice.addEventListener("click", e => {
-        if(!acceptingAnswers) return
+        if (!acceptingAnswers) return
 
         acceptingAnswers = false
         const selectedChoice = e.target
@@ -138,7 +250,7 @@ choices.forEach(choice => {
 
         let classToApply = selectedAnswer == currentQuestion.answer ? "correct" : "incorrect"
 
-        if(classToApply === "correct") {
+        if (classToApply === "correct") {
             incrementScore(SCORE_POINTS)
         }
 
@@ -149,13 +261,15 @@ choices.forEach(choice => {
             getNewQuestion()
 
         }, 1000)
-    
+
     })
 })
 
 incrementScore = num => {
-    score +=num
+    score += num
     scoreText.innerText = score
 }
 
 startGame()
+
+// selectLevel()
